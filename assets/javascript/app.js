f051c2ae50102cdf52f2aee44b76b359f5f70c31
Kyle Brittan <kabrittan@gmail.com>
2017-05-19 22:31:05 -0700

    Update JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+// Initial array
+    var topics = ["dogs","cats","birds", "fish"];
+
+  // Capture topic name
+  function displayTopicName() {        
+    
+    $("#animalButtons").on("click", function() {  
+       
+       var topicName = $(this).attr("animal-input");
+       var queryURL =  "http://api.giphy.com/v1/gifs/search?q=" + topicName + "&api_key=dc6zaTOxFJmzC&limit=10";     
+
+          $.ajax({
+            url: queryURL,
+            method: "GET"  
+          })
+          .done(function(response) {
+          console.log(response);
+            var results = response.data;
+            $("#animals").empty();
+            $(".item").empty();
+
+            for (var i = 0; i < results.length; i++) {
+              var gifDiv = $("<div class='item'>");
+
+              var rating = results[i].rating;
+
+              var p = $("<p>").text("Rating: " + rating);
+
+              var topicImage = $("<img>");
+              topicImage.attr("src", results[i].images.fixed_height_still.url);
+              topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+              topicImage.attr("data-animate", results[i].images.fixed_height.url);
+              topicImage.attr("data-state", "still");
+              topicImage.attr("class", "gif");
+
+              gifDiv.append(p);
+              gifDiv.append(topicImage);
+              $("#animals").append(gifDiv);
+              
+            } 
+          pauseGifs();
+          });        
+    });
+
+  };
+
